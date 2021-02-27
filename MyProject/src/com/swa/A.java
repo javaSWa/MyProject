a2543f8ddb5afb259fe4de46259b450122bd8a9a
CITY <CITY@DESKTOP-FJ6AFH9>
2021-02-27 21:46:57 +0530

    branchX

--- MyProject/src/com/swa/A.java
+++ MyProject/src/com/swa/A.java
@@ -1,5 +1,11 @@
 package com.swa;
 
 public class A {
+	
+	public void m1() {
+		
+		System.out.println("hii...added methods");
+		
+	}
 
 }
---
+++ MyProject/src/com/swa/B.java
@@ -0,0 +1,9 @@
+package com.swa;
+
+public class B {
+
+	public void m2() {
+		
+		System.out.println("hellooooooo");
+	}
+}
